52d01d15ee2d14dc27b625d147c848ef3e162db3
StackDoubleFlow <ojaslandge@gmail.com>
2019-11-29 14:52:42 -0600

    Load IDT and catch breakpoint exception

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 #![no_std]
 #![no_main]
+#![feature(abi_x86_interrupt)]
 
 use core::panic::PanicInfo;
 
 mod vga;
+mod interrupts;
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
@@ -11,9 +13,16 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
+fn init() {
+    interrupts::init_idt();
+}
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    init();
     println!("Hello World!, here are some numbers: {} {}", 123, 3.14159);
+    x86_64::instructions::interrupts::int3();
+    println!("works");
 
     loop {}
 }
