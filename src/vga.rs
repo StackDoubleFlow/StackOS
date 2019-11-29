e1d69a554fd46dda1c70b8195f427d87e85d8953
StackDoubleFlow <ojaslandge@gmail.com>
2019-11-29 14:17:37 -0600

    Implement new_line

--- src/vga.rs
+++ src/vga.rs
@@ -86,7 +86,18 @@ impl Writer {
         }
     }
 
-    fn new_line(&mut self) {/* TODO */}
+    fn new_line(&mut self) {
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+    }
+
+    fn clear_row(&mut self, row: usize) {/* TODO */}
 }
 
 impl fmt::Write for Writer {
