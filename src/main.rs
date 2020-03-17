ce63288b459d0138ec482d72cbc1c8c3e7667dfd
StackDoubleFlow <ojaslandge@gmail.com>
2020-03-17 17:17:24 -0500

    Heap allocation using the linked_list_allocator crate

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,29 @@
 #![no_std]
 #![no_main]
 #![feature(abi_x86_interrupt)]
+#![feature(alloc_error_handler)]
 
-use bootloader::{entry_point, BootInfo};
-use core::panic::PanicInfo;
+extern crate alloc;
 
 mod gdt;
 mod interrupts;
 mod memory;
 mod vga;
+mod allocator;
+
+use bootloader::{entry_point, BootInfo};
+use core::panic::PanicInfo;
+
 
 pub fn hlt() -> ! {
     loop {
         x86_64::instructions::hlt();
     }
+}
+
+#[alloc_error_handler]
+fn alloc_error_handler(layout: alloc::alloc::Layout) -> ! {
+    panic!("allocation error: {:?}", layout)
 }
 
 #[panic_handler]
@@ -23,19 +33,32 @@ fn panic(info: &PanicInfo) -> ! {
 }
 
 fn init() {
+    use memory::{self, BootInfoFrameAllocator};
+    use x86_64::VirtAddr;
+    
     interrupts::init_idt();
     gdt::init();
     unsafe { interrupts::PICS.lock().initialize() };
     x86_64::instructions::interrupts::enable();
+
 }
 
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use alloc::boxed::Box;
     use memory::{self, BootInfoFrameAllocator};
-    use x86_64::{structures::paging::Page, VirtAddr};
+    use x86_64::VirtAddr;
 
     init();
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
+
+    let b = Box::new(41);
+    println!("Box address is: {:p}", b);
 
     println!("Hello World!, here are some numbers: {} {}", 123, 3.14159);
 
