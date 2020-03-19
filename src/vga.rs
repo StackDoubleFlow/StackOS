eaee335ee60bd4750b81b2794e3344d6577d2f55
StackDoubleFlow <ojaslandge@gmail.com>
2020-03-18 21:19:26 -0500

    Use `vga` crate

--- src/vga.rs
+++ src/vga.rs
@@ -2,6 +2,10 @@ use core::fmt;
 use lazy_static::lazy_static;
 use spin::Mutex;
 use volatile::Volatile;
+use vga::{
+    writers::{Text80x25, TextWriter, ScreenCharacter},
+    colors::{Color16Bit, TextModeColor},
+};
 
 #[macro_export]
 macro_rules! print {
@@ -24,62 +28,18 @@ pub fn _print(args: fmt::Arguments) {
     });
 }
 
-enum Color {
-    Black = 0,
-    Blue = 1,
-    Green = 2,
-    Cyan = 3,
-    Red = 4,
-    Magenta = 5,
-    Brown = 6,
-    LightGray = 7,
-    DarkGrey = 8,
-    LightBlue = 9,
-    LightGreen = 10,
-    LightCyan = 11,
-    LightRed = 12,
-    Pink = 13,
-    Yellow = 14,
-    White = 15,
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(transparent)]
-struct ColorCode(u8);
-
-impl ColorCode {
-    fn new(foreground: Color, background: Color) -> ColorCode {
-        ColorCode((background as u8) << 4 | (foreground as u8))
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(C)]
-struct ScreenChar {
-    ascii_character: u8,
-    color_code: ColorCode,
-}
-
-const BUFFER_HEIGHT: usize = 25;
-const BUFFER_WIDTH: usize = 80;
-
-#[repr(transparent)]
-struct Buffer {
-    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
-}
-
 lazy_static! {
     pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
         column_position: 0,
-        color_code: ColorCode::new(Color::Yellow, Color::Black),
-        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+        color_code: TextModeColor::new(Color16Bit::Yellow, Color16Bit::Black),
+        text_mode: Text80x25::new(),
     });
 }
 
 pub struct Writer {
     column_position: usize,
-    color_code: ColorCode,
-    buffer: &'static mut Buffer,
+    color_code: TextModeColor,
+    text_mode: Text80x25,
 }
 
 impl Writer {
@@ -87,18 +47,15 @@ impl Writer {
         match byte {
             b'\n' => self.new_line(),
             byte => {
-                if self.column_position >= BUFFER_WIDTH {
+                if self.column_position >= self.text_mode.get_width() {
                     self.new_line();
                 }
 
-                let row = BUFFER_HEIGHT - 1;
+                let row = self.text_mode.get_height() - 1;
                 let col = self.column_position;
 
                 let color_code = self.color_code;
-                self.buffer.chars[row][col].write(ScreenChar {
-                    ascii_character: byte,
-                    color_code: color_code,
-                });
+                self.text_mode.write_character(col, row, ScreenCharacter::new(byte, color_code));
                 self.column_position += 1;
             }
         }
@@ -116,23 +73,22 @@ impl Writer {
     }
 
     fn new_line(&mut self) {
-        for row in 1..BUFFER_HEIGHT {
-            for col in 0..BUFFER_WIDTH {
-                let character = self.buffer.chars[row][col].read();
-                self.buffer.chars[row - 1][col].write(character);
+        let width = self.text_mode.get_width();
+        let height = self.text_mode.get_height();
+        for row in 1..height {
+            for col in 0..width {
+                let character = self.text_mode.read_character(col, row);
+                self.text_mode.write_character(col, row - 1, character);
             }
         }
-        self.clear_row(BUFFER_HEIGHT - 1);
+        self.clear_row(height - 1);
         self.column_position = 0;
     }
 
     fn clear_row(&mut self, row: usize) {
-        let blank = ScreenChar {
-            ascii_character: b' ',
-            color_code: self.color_code,
-        };
-        for col in 0..BUFFER_WIDTH {
-            self.buffer.chars[row][col].write(blank);
+        let blank = ScreenCharacter::new(b' ', self.color_code);
+        for col in 0..self.text_mode.get_width() {
+            self.text_mode.write_character(col, row, blank);
         }
     }
 }
